ae53afd54cba0992da918e2b0099242c81f1ae05
kawapa <kawapa@outlook.com>
2025-01-13 23:05:16 +0100

    Improve post-create.sh

--- post-create.sh
+++ post-create.sh
@@ -2,6 +2,8 @@ sed -i 's/^# gem "/gem "/' Gemfile
 sed -i 's/^# theme: just-the-docs/theme: just-the-docs/' _config.yml
 sed -i 's/^remote_theme: kawapa\/just-the-docs/# remote_theme: kawapa\/just-the-docs/' _config.yml
 
+git config --global --add safe.directory /srv/jekyll/just-the-docs
+
 if [ -f Gemfile.lock ] && grep "BUNDLED WITH" Gemfile.lock > /dev/null; then
     cat Gemfile.lock | tail -n 2 | grep -C2 "BUNDLED WITH" | tail -n 1 | xargs gem install bundler
 fi
